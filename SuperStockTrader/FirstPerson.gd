9cb861d424b38616ce0f8244e94a28ad61249785
Mike149x <mikeismike148@gmail.com>
2021-05-10 11:37:50 +1200

    test
    testing

--- SuperStockTrader/FirstPerson.gd
+++ SuperStockTrader/FirstPerson.gd
@@ -27,13 +27,29 @@ func _input(event):
 
 func _process(delta):
 	#this function updates every frame
+	
+	direction = Vector3()
+	#This resets speed to zero when nothing pressed
+	
 	if Input.is_action_just_pressed("ui_cancel"):
 		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+		
+		#when clicking ESC, mouse appears so user can exit
 	
 	if Input.is_action_pressed("move_forward"):
 		direction -= transform.basis.z
-	elif Input.is_action_just_pressed("move_backward"):
+	
+	elif Input.is_action_pressed("move_backward"):
 		direction += transform.basis.z
+	
+	elif Input.is_action_pressed("move_left"):
+		direction -= transform.basis.x
+			
+	elif Input.is_action_pressed("move_right"):
+		direction += transform.basis.x
+	
+	direction = direction.normalized()
+	move_and_slide(direction * speed, Vector3.UP)
 	
 
 
