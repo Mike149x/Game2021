0a7d12089d452d5e8fa9ba519143c52f2c0de671
Mike149x <mikeismike148@gmail.com>
2021-05-25 10:56:21 +1200

    Raycast Shotgun

--- SuperStockTrader/Shotgun.gd
+++ SuperStockTrader/Shotgun.gd
@@ -2,7 +2,7 @@ extends Weapon
 
 export var fire_range = 10
 
-onready var raycast = $"../Head/Camera/RayCast"
+
 
 func _ready():
 	raycast.cast_to = Vector3(0, 0, -fire_range)
--- SuperStockTrader/Weapon.gd
+++ SuperStockTrader/Weapon.gd
@@ -6,12 +6,22 @@ export var fire_rate = 0.5
 export var clip_size = 5
 export var reload_rate = 1
 
+onready var ammo_label = $"/root/MainScene/UI/Label"
 onready var raycast = $"../Head/Camera/RayCast"
-var current_ammo = clip_size
+var current_ammo = 0
 var can_fire = true
 var reloading = false
 
+
+func _ready():
+	current_ammo = clip_size
+
 func _process(delta):
+	if reloading:
+		ammo_label.set_text("Reloading...")
+	else:
+		ammo_label.set_text("%d / %d" % [current_ammo, clip_size])
+	
 	if Input.is_action_just_pressed("primary_fire") and can_fire:
 		#fires the weapon
 		if current_ammo > 0 and not reloading:
