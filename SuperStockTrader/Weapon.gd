3be40ee26e1ba9fa4fd586dc917150d120adeb79
Mike149x <mikeismike148@gmail.com>
2021-05-24 18:59:46 +1200

    Raycast Big Progress

--- SuperStockTrader/Weapon.gd
+++ SuperStockTrader/Weapon.gd
@@ -1,11 +1,12 @@
 extends Node
 
+class_name Weapon
 
 export var fire_rate = 0.5
 export var clip_size = 5
 export var reload_rate = 1
 
-onready var raycast = $"..Head/Camera/Raycast"
+onready var raycast = $"../Head/Camera/RayCast"
 var current_ammo = clip_size
 var can_fire = true
 var reloading = false
@@ -17,22 +18,30 @@ func _process(delta):
 			print("Fired weapon")
 			can_fire = false
 			current_ammo -= 1
+			#after firing, ammo minus 1
 			check_collision()
 		
 			yield(get_tree().create_timer(fire_rate), "timeout")
+			#creates timer, waits before fire_rate, then fires
 		
 			can_fire = true
 		elif not reloading:
+			#so if ammo is less than 1 and NOT reloading, the gun will reload
 			print("Reloading...")
 			reloading = true
 			yield(get_tree().create_timer(reload_rate), "timeout")
+			#creates timer, waits before reload_rate, then fires
 			current_ammo = clip_size
 			reloading = false
 			print("Reload Complete")
 			
 func check_collision():
 	if raycast.is_colliding():
-		var collider = raycast.get_collider
+		var collider = raycast.get_collider()
+		if collider.is_in_group("Enemies"):
+			collider.queue_free()
+			print("Killed" + collider.name)
+			
 
 
 
