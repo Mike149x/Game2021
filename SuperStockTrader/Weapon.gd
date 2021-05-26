eec7d7cdfb48cd202e05c9edb1f794f55ed58153
Mike149x <mikeismike148@gmail.com>
2021-05-26 12:24:49 +1200

    Tidying up code

--- SuperStockTrader/Weapon.gd
+++ SuperStockTrader/Weapon.gd
@@ -22,37 +22,46 @@ func _process(delta):
 	else:
 		ammo_label.set_text("%d / %d" % [current_ammo, clip_size])
 	
-	if Input.is_action_just_pressed("primary_fire") and can_fire:
-		#fires the weapon
+	if Input.is_action_pressed("primary_fire") and can_fire:
 		if current_ammo > 0 and not reloading:
-			print("Fired weapon")
-			can_fire = false
-			current_ammo -= 1
-			#after firing, ammo minus 1
-			check_collision()
-		
-			yield(get_tree().create_timer(fire_rate), "timeout")
-			#creates timer, waits before fire_rate, then fires
-		
-			can_fire = true
+			fire()
+				#fires the weapon
 		elif not reloading:
 			#so if ammo is less than 1 and NOT reloading, the gun will reload
-			print("Reloading...")
-			reloading = true
-			yield(get_tree().create_timer(reload_rate), "timeout")
-			#creates timer, waits before reload_rate, then fires
-			current_ammo = clip_size
-			reloading = false
-			print("Reload Complete")
-			
+			reload()
+	
+	if Input.is_action_just_pressed("reload") and not reloading:
+		reload()
+		
 func check_collision():
 	if raycast.is_colliding():
 		var collider = raycast.get_collider()
 		if collider.is_in_group("Enemies"):
 			collider.queue_free()
 			print("Killed" + collider.name)
-			
 
+func fire():
+	print("Fired weapon")
+	can_fire = false
+	current_ammo -= 1
+	#after firing, ammo minus 1
+	check_collision()
+		
+	yield(get_tree().create_timer(fire_rate), "timeout")
+	#creates timer, waits before fire_rate, then fires
+		
+	can_fire = true
+	
+
+func reload():
+	print("Reloading...")
+	reloading = true
+	yield(get_tree().create_timer(reload_rate), "timeout")
+	#creates timer, waits before reload_rate, then fires
+	current_ammo = clip_size
+	reloading = false
+	print("Reload Complete")
+	
 
 
 
