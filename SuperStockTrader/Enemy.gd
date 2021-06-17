21ef0a451c6d38e8614b661714d7a87937924548
Mike149x <mikeismike148@gmail.com>
2021-06-18 09:57:00 +1200

    enemy now moves

--- SuperStockTrader/Enemy.gd
+++ SuperStockTrader/Enemy.gd
@@ -1,6 +1,6 @@
 extends KinematicBody
 
-
+export var speed = 100
 var space_state
 var target
 
@@ -11,7 +11,12 @@ func _ready():
 func _process(delta):
 	if target:
 		var result = space_state.intersect_ray(global_transform.origin, target.global_transform.origin)
-		look_at(target.global_transform.origin, Vector3.UP)
+		if result.collider.is_in_group("Player"):
+			look_at(target.global_transform.origin, Vector3.UP)
+			set_color_red()
+			move_to_target(delta)
+		else:
+			set_color_green()
 
 func _on_Area_body_entered(body):
 	if body.is_in_group("Player"):
@@ -24,6 +29,10 @@ func _on_Area_body_exited(body):
 		target = null
 		print(body.name + " exited")
 		set_color_green()  
+func move_to_target(delta):
+	var direction = (target.transform.origin - transform.origin).normalized()
+	move_and_slide(direction * speed * delta, Vector3.UP) 
+
 
 func set_color_red():
 	$MeshInstance.get_surface_material(0).set_albedo(Color(1, 0, 0))
